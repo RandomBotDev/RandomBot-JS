1bce938be39542669f829b122a90d2a14b3ca5a7
Da Guacaplushy <hairy.furball154@gmail.com>
2021-02-16 16:51:48 +0000

    add goto normal website button

--- web/js/buttons.js
+++ web/js/buttons.js
@@ -3,13 +3,15 @@ function btncon() {
   btninv = document.getElementById("btninv")
   btnhelp = document.getElementById("btnhelp")
   btnslash = document.getElementById("btnslash")
+  btnnormal = document.getElementById("btnnormal")
   btnmain = document.getElementById("btnmain")
   if (btndiv.style.width === "120px") {
     btndiv.style.width = "200px"; 
-    btndiv.style.height = "200px";
+    btndiv.style.height = "250px";
     btninv.style.visibility = "visible";
     btnhelp.style.visibility = "visible";
     btnslash.style.visibility = "visible";
+    btnnormal.style.visibility = "visible";
     btnmain.innerHTML = "Hide buttons";
   } else {
     btndiv.style.width = "120px"; 
@@ -17,6 +19,7 @@ function btncon() {
     btninv.style.visibility = "hidden";
     btnhelp.style.visibility = "hidden";
     btnslash.style.visibility = "hidden";
+    btnnormal.style.visibility = "hidden";
     btnmain.innerHTML = "Show buttons";
   }
 }
@@ -25,7 +28,9 @@ function btnload() {
   btninv = document.getElementById("btninv")
   btnhelp = document.getElementById("btnhelp")
   btnslash = document.getElementById("btnslash")
+  btnnormal = document.getElementById("btnnormal")
   btninv.style.visibility = "hidden";
   btnhelp.style.visibility = "hidden";
   btnslash.style.visibility = "hidden";
+  btnnormal.style.visibility = "hidden";
 }
